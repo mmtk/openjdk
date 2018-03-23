a89facf01a9ff45e0fe9bc8e71de728ad8b20b25
Brenda Wang <u6374399@anu.edu.au>
2018-03-23 18:02:25 +1100

    Added some mmtk functions to interface

--- src/hotspot/share/gc/mmtk/mmtkHeap.cpp
+++ src/hotspot/share/gc/mmtk/mmtkHeap.cpp
@@ -53,12 +53,12 @@ object iterator??!!
 
 jint MMTkHeap::initialize() {
 
-   CollectedHeap::pre_initialize();
+    CollectedHeap::pre_initialize();
 
     const size_t heap_size = collector_policy()->max_heap_byte_size();
     printf("policy max heap size %d, min heap size %d\n", heap_size, collector_policy()->min_heap_byte_size());
-   size_t mmtk_heap_size = heap_size;
-   /*forcefully*/ //mmtk_heap_size = (1<<31) -1;
+    size_t mmtk_heap_size = heap_size;
+    /*forcefully*/ //mmtk_heap_size = (1<<31) -1;
 
     gc_init(mmtk_heap_size);
 
@@ -66,24 +66,26 @@ jint MMTkHeap::initialize() {
 
     printf("inside mmtkHeap.cpp.. reserved base %x size %u \n", heap_rs.base(), heap_rs.size());
 
-  os::trace_page_sizes("Heap",
-                       _collector_policy->min_heap_byte_size(),
-                       mmtk_heap_size,
-                       collector_policy()->space_alignment(),
-                       heap_rs.base(),
-                       heap_rs.size());
+    os::trace_page_sizes("Heap",
+                         _collector_policy->min_heap_byte_size(),
+                         mmtk_heap_size,
+                         collector_policy()->space_alignment(),
+                         heap_rs.base(),
+                         heap_rs.size());
+    initialize_reserved_region((HeapWord*)heap_rs.base(), (HeapWord*)(heap_rs.base() + heap_rs.size()));
 
-  initialize_reserved_region((HeapWord*)heap_rs.base(), (HeapWord*)(heap_rs.base() + heap_rs.size()));
+    //_start = (HeapWord*)heap_rs.base();
+    //_end = (HeapWord*)(heap_rs.base() + heap_rs.size());
 
-  _start = (HeapWord*)heap_rs.base();
-  _end = (HeapWord*)(heap_rs.base() + heap_rs.size());
+    _start = (HeapWord*) starting_heap_address();
+    _end = (HeapWord*) last_heap_address();
 
-  NoBarrier* const barrier_set = new NoBarrier(reserved_region());
-  //barrier_set->initialize();
-  set_barrier_set(barrier_set);
+    NoBarrier* const barrier_set = new NoBarrier(reserved_region());
+    //barrier_set->initialize();
+    set_barrier_set(barrier_set);
 
     // Set up the GCTaskManager
-  //  _mmtk_gc_task_manager = mmtkGCTaskManager::create(ParallelGCThreads);
+    //  _mmtk_gc_task_manager = mmtkGCTaskManager::create(ParallelGCThreads);
 
     printf("inside mmtkHeap.cpp after initialization with size %d\n", mmtk_heap_size);
     return JNI_OK;
@@ -94,241 +96,234 @@ HeapWord* MMTkHeap::mem_allocate(size_t size, bool* gc_overhead_limit_was_exceed
 
     Thread* thread = Thread::current();
     // For mmtk support
-      if(thread->mmtk_mutator()==NULL){
-          printf("Setting mutator for thread %p id: %u \n", thread, thread->self_raw_id());
+    if(thread->mmtk_mutator()==NULL){
+        printf("Setting mutator for thread %p id: %u \n", thread, thread->self_raw_id());
         thread->set_mmtk_mutator(thread->self_raw_id());
-      }
-      if(thread->mmtk_mutator()==(void*)(0xf1f1f1f1f1f1f1f1)){
-          printf("Setting mutator for thread %p id: %u \n", thread, thread->self_raw_id());
+    }
+    if(thread->mmtk_mutator()==(void*)(0xf1f1f1f1f1f1f1f1)){
+        printf("Setting mutator for thread %p id: %u \n", thread, thread->self_raw_id());
         thread->set_mmtk_mutator(thread->self_raw_id());
-      }
-
+    }
 
     void* obj_ptr = alloc(Thread::current()->mmtk_mutator(), size*HeapWordSize, 1, 0, 0);
     HeapWord* obj = (HeapWord*) obj_ptr;
-    printf("Allocated pointer obj_ptr %p obj %p \n", obj_ptr, obj);
+    //printf("Allocated pointer obj_ptr %p obj %p \n", obj_ptr, obj);
     if (obj != NULL) {
-      return obj;
+        return obj;
     }
     // Otherwise...
     return (HeapWord*) alloc_slow(Thread::current()->mmtk_mutator(), size*HeapWordSize, 1, 0, 0);
 }
 
-
 void MMTkHeap::post_initialize() {
-  CollectedHeap::post_initialize();
+    CollectedHeap::post_initialize();
 }
-
-
-
 
 ////Previously pure abstract methods--
 
-   size_t MMTkHeap::capacity() const {
-       return max_capacity();
-   }
+size_t MMTkHeap::capacity() const {
+   return max_capacity();
+}
 
-    size_t MMTkHeap::max_capacity() const {
-       //used by jvm
+size_t MMTkHeap::max_capacity() const {
+   //used by jvm
 
-       // Support for java.lang.Runtime.maxMemory():  return the maximum amount of
-       // memory that the vm could make available for storing 'normal' java objects.
-       // This is based on the reserved address space, but should not include space
-       // that the vm uses internally for bookkeeping or temporary storage
-       // (e.g., in the case of the young gen, one of the survivor spaces).
+   // Support for java.lang.Runtime.maxMemory():  return the maximum amount of
+   // memory that the vm could make available for storing 'normal' java objects.
+   // This is based on the reserved address space, but should not include space
+   // that the vm uses internally for bookkeeping or temporary storage
+   // (e.g., in the case of the young gen, one of the survivor spaces).
 
-       return openjdk_max_capacity();
-   }
+   return openjdk_max_capacity();
+}
 
+size_t MMTkHeap::used() const {
+   //has to be implemented. used in universe.cpp
+   //in ps : young_gen()->used_in_bytes() + old_gen()->used_in_bytes()
+   //guarantee(false, "error not yet implemented");
+   return used_bytes();
+}
 
+bool MMTkHeap::is_maximal_no_gc() const {
+   //has to be implemented. used in collectorpolicy.cpp in shared
 
-   size_t MMTkHeap::used() const {
-       //has to be implemented. used in universe.cpp
-       //in ps : young_gen()->used_in_bytes() + old_gen()->used_in_bytes()
-       //guarantee(false, "error not yet implemented");
-       return used_bytes();
-   }
+   // Return "true" if the part of the heap that allocates Java
+   // objects has reached the maximal committed limit that it can
+   // reach, without a garbage collection.
 
-   bool MMTkHeap::is_maximal_no_gc() const {
-       //has to be implemented. used in collectorpolicy.cpp in shared
-
-       // Return "true" if the part of the heap that allocates Java
-       // objects has reached the maximal committed limit that it can
-       // reach, without a garbage collection.
-
-       //can be implemented like if(used()>= capacity()-X){}
-       return false;
-   }
-
-
-
-   bool MMTkHeap::is_in(const void* p) const {
-       //used in collected heap , jvmruntime and many more.........
-
-       // Returns "TRUE" iff "p" points into the committed areas of the heap.
-       //we need starting and endinf address of the heap
-
-       // in ps : char* const cp = (char*)p;
-       //return cp >= committed_low_addr() && cp < committed_high_addr();
-
-       guarantee(false, "is in not supported");
-       return false;
-   }
-
-    bool MMTkHeap::is_in_reserved(const void* p) const {
-        //used in collected heap , jvmruntime and many more.........
-
-        // Returns "TRUE" iff "p" points into the committed areas of the heap.
-        //we need starting and endinf address of the heap
-
-        // in ps : char* const cp = (char*)p;
-        //return cp >= committed_low_addr() && cp < committed_high_addr();
-
-        //guarantee(false, "is in not supported");
-        return true;
-    }
-
-    bool MMTkHeap::supports_tlab_allocation() const {
-        //returning false is good enough...used in universe.cpp
-        return false;
-    }
-
-  // The amount of space available for thread-local allocation buffers.
-   size_t MMTkHeap::tlab_capacity(Thread *thr) const {
-       //no need to further implement but we need UseTLAB=False
-       guarantee(false, "tlab_capacity not supported");
-       return 0;
-   }
-
-  // The amount of used space for thread-local allocation buffers for the given thread.
-   size_t MMTkHeap::tlab_used(Thread *thr) const {
-       //no need to further implement but we need UseTLAB=False
-       guarantee(false, "tlab_used not supported");
-       return 0;
-   }
-
-
-   // Can a compiler initialize a new object without store barriers?
-  // This permission only extends from the creation of a new object
-  // via a TLAB up to the first subsequent safepoint. //However, we will not use tlab
-   bool MMTkHeap::can_elide_tlab_store_barriers() const {  //OK
-       return true;
-   }
-
-
-   bool MMTkHeap::can_elide_initializing_store_barrier(oop new_obj) { //OK
-       //guarantee(false, "can elide initializing store barrier not supported");
-       return false;
-   }
-
-  // mark to be thus strictly sequenced after the stores.
-   bool MMTkHeap::card_mark_must_follow_store() const { //OK
-       return false;
-   }
-
-   void MMTkHeap::collect(GCCause::Cause cause) {//later when gc is implemented in rust
-       guarantee(false, "collect not supported");
-   }
-
-  // Perform a full collection
-   void MMTkHeap::do_full_collection(bool clear_all_soft_refs) {//later when gc is implemented in rust
-       guarantee(false, "do full collection not supported");
-   }
-
-
-  // Return the CollectorPolicy for the heap
-   CollectorPolicy* MMTkHeap::collector_policy() const {return _collector_policy;}//OK
-
-   GrowableArray<GCMemoryManager*> MMTkHeap::memory_managers() {//may cause error
-
-       GrowableArray<GCMemoryManager*> memory_managers(1);
-       memory_managers.append(_mmtk_manager);
-       return memory_managers;
-   }
-   GrowableArray<MemoryPool*> MMTkHeap::memory_pools() {//may cause error
-
-       GrowableArray<MemoryPool*> memory_pools(1);
-       memory_pools.append(_mmtk_pool);
-       return memory_pools;
-   }
-
-  // Iterate over all objects, calling "cl.do_object" on each.
-   void MMTkHeap::object_iterate(ObjectClosure* cl) { //No need to implement.Traced whole path.Only other heaps call it.
-       guarantee(false, "object iterate not supported");
-   }
-
-  // Similar to object_iterate() except iterates only
-  // over live objects.
-   void MMTkHeap::safe_object_iterate(ObjectClosure* cl) { //not sure..many dependencies from vm
-       guarantee(false, "safe object iterate not supported");
-   }
-
-   HeapWord* MMTkHeap::block_start(const void* addr) const {//OK
-       guarantee(false, "block start not supported");
-       return NULL;
-   }
-
-   size_t MMTkHeap::block_size(const HeapWord* addr) const { //OK
-       guarantee(false, "block size not supported");
-       return 0;
-   }
-
-   bool MMTkHeap::block_is_obj(const HeapWord* addr) const { //OK
-       guarantee(false, "block is obj not supported");
+   //can be implemented like if(used()>= capacity()-X){}
    return false;
-   }
+}
 
-   jlong MMTkHeap::millis_since_last_gc() {//later when gc is implemented in rust
-       guarantee(false, "time since last gc not supported");
-       return 0;
-   }
+bool MMTkHeap::is_in(const void* p) const {
+   //used in collected heap , jvmruntime and many more.........
 
+   // Returns "TRUE" iff "p" points into the committed areas of the heap.
+   //we need starting and endinf address of the heap
 
-   void MMTkHeap::prepare_for_verify() {
-       guarantee(false, "prepare for verify not supported");
-   }
+   // in ps : char* const cp = (char*)p;
+   //return cp >= committed_low_addr() && cp < committed_high_addr();
 
+   //guarantee(false, "is in not supported");
+    return p > (void*) _start && p < (void*) _end;
+}
 
-   void MMTkHeap::initialize_serviceability() {//OK
+bool MMTkHeap::is_in_reserved(const void* p) const {
+    //used in collected heap , jvmruntime and many more.........
 
+    // Returns "TRUE" iff "p" points into the committed areas of the heap.
+    //we need starting and endinf address of the heap
 
-       _mmtk_pool = new MMTkMemoryPool(_start, _end,  "MMTk pool",collector_policy()->min_heap_byte_size(), false);
+    // in ps : char* const cp = (char*)p;
+    //return cp >= committed_low_addr() && cp < committed_high_addr();
 
-         _mmtk_manager = new GCMemoryManager("MMTk GC", "end of GC");
-         _mmtk_manager->add_pool(_mmtk_pool);
-   }
+    //guarantee(false, "is in not supported");
 
-  // Print heap information on the given outputStream.
-   void MMTkHeap::print_on(outputStream* st) const {guarantee(false, "print on not supported");}
+    //printf("calling MMTkHeap::is_in_reserved\n");
+    return p > (void*) _start && p < (void*) _end;
+}
 
+bool MMTkHeap::supports_tlab_allocation() const {
+    //returning false is good enough...used in universe.cpp
+    return false;
+}
 
-  // Print all GC threads (other than the VM thread)
-  // used by this heap.
-   void MMTkHeap::print_gc_threads_on(outputStream* st) const {guarantee(false, "print gc threads on not supported");}
+// The amount of space available for thread-local allocation buffers.
+size_t MMTkHeap::tlab_capacity(Thread *thr) const {
+   //no need to further implement but we need UseTLAB=False
+   guarantee(false, "tlab_capacity not supported");
+   return 0;
+}
 
-  // Iterator for all GC threads (other than VM thread)
-   void MMTkHeap::gc_threads_do(ThreadClosure* tc) const {
-     // guarantee(false, "gc threads do not supported");
-   }
-
-  // Print any relevant tracing info that flags imply.
-  // Default implementation does nothing.
-   void MMTkHeap::print_tracing_info() const {
-    //guarantee(false, "print tracing info not supported");
-   }
-
-
-  // An object is scavengable if its location may move during a scavenge.
-  // (A scavenge is a GC which is not a full GC.)
-   bool MMTkHeap::is_scavengable(oop obj) {return false;}
-  // Registering and unregistering an nmethod (compiled code) with the heap.
-  // Override with specific mechanism for each specialized heap type.
-
-  // Heap verification
-   void MMTkHeap::verify(VerifyOption option) {guarantee(false, "verify not supported");}
+// The amount of used space for thread-local allocation buffers for the given thread.
+size_t MMTkHeap::tlab_used(Thread *thr) const {
+   //no need to further implement but we need UseTLAB=False
+   guarantee(false, "tlab_used not supported");
+   return 0;
+}
 
 
-   /*
-    * files with prints currently:
-    * collectedHeap.inline.hpp, mmtkHeap.cpp,
-    */
+// Can a compiler initialize a new object without store barriers?
+// This permission only extends from the creation of a new object
+// via a TLAB up to the first subsequent safepoint. //However, we will not use tlab
+bool MMTkHeap::can_elide_tlab_store_barriers() const {  //OK
+   return true;
+}
+
+
+bool MMTkHeap::can_elide_initializing_store_barrier(oop new_obj) { //OK
+   //guarantee(false, "can elide initializing store barrier not supported");
+   return false;
+}
+
+// mark to be thus strictly sequenced after the stores.
+bool MMTkHeap::card_mark_must_follow_store() const { //OK
+   return false;
+}
+
+void MMTkHeap::collect(GCCause::Cause cause) {//later when gc is implemented in rust
+   guarantee(false, "collect not supported");
+}
+
+// Perform a full collection
+void MMTkHeap::do_full_collection(bool clear_all_soft_refs) {//later when gc is implemented in rust
+   guarantee(false, "do full collection not supported");
+}
+
+
+// Return the CollectorPolicy for the heap
+CollectorPolicy* MMTkHeap::collector_policy() const {return _collector_policy;}//OK
+
+GrowableArray<GCMemoryManager*> MMTkHeap::memory_managers() {//may cause error
+
+   GrowableArray<GCMemoryManager*> memory_managers(1);
+   memory_managers.append(_mmtk_manager);
+   return memory_managers;
+}
+GrowableArray<MemoryPool*> MMTkHeap::memory_pools() {//may cause error
+
+   GrowableArray<MemoryPool*> memory_pools(1);
+   memory_pools.append(_mmtk_pool);
+   return memory_pools;
+}
+
+// Iterate over all objects, calling "cl.do_object" on each.
+void MMTkHeap::object_iterate(ObjectClosure* cl) { //No need to implement.Traced whole path.Only other heaps call it.
+   guarantee(false, "object iterate not supported");
+}
+
+// Similar to object_iterate() except iterates only
+// over live objects.
+void MMTkHeap::safe_object_iterate(ObjectClosure* cl) { //not sure..many dependencies from vm
+   guarantee(false, "safe object iterate not supported");
+}
+
+HeapWord* MMTkHeap::block_start(const void* addr) const {//OK
+   guarantee(false, "block start not supported");
+   return NULL;
+}
+
+size_t MMTkHeap::block_size(const HeapWord* addr) const { //OK
+   guarantee(false, "block size not supported");
+   return 0;
+}
+
+bool MMTkHeap::block_is_obj(const HeapWord* addr) const { //OK
+   guarantee(false, "block is obj not supported");
+return false;
+}
+
+jlong MMTkHeap::millis_since_last_gc() {//later when gc is implemented in rust
+   guarantee(false, "time since last gc not supported");
+   return 0;
+}
+
+
+void MMTkHeap::prepare_for_verify() {
+   guarantee(false, "prepare for verify not supported");
+}
+
+
+void MMTkHeap::initialize_serviceability() {//OK
+
+
+   _mmtk_pool = new MMTkMemoryPool(_start, _end,  "MMTk pool",collector_policy()->min_heap_byte_size(), false);
+
+     _mmtk_manager = new GCMemoryManager("MMTk GC", "end of GC");
+     _mmtk_manager->add_pool(_mmtk_pool);
+}
+
+// Print heap information on the given outputStream.
+void MMTkHeap::print_on(outputStream* st) const {guarantee(false, "print on not supported");}
+
+
+// Print all GC threads (other than the VM thread)
+// used by this heap.
+void MMTkHeap::print_gc_threads_on(outputStream* st) const {guarantee(false, "print gc threads on not supported");}
+
+// Iterator for all GC threads (other than VM thread)
+void MMTkHeap::gc_threads_do(ThreadClosure* tc) const {
+ // guarantee(false, "gc threads do not supported");
+}
+
+// Print any relevant tracing info that flags imply.
+// Default implementation does nothing.
+void MMTkHeap::print_tracing_info() const {
+//guarantee(false, "print tracing info not supported");
+}
+
+
+// An object is scavengable if its location may move during a scavenge.
+// (A scavenge is a GC which is not a full GC.)
+bool MMTkHeap::is_scavengable(oop obj) {return false;}
+// Registering and unregistering an nmethod (compiled code) with the heap.
+// Override with specific mechanism for each specialized heap type.
+
+// Heap verification
+void MMTkHeap::verify(VerifyOption option) {guarantee(false, "verify not supported");}
+
+
+/*
+* files with prints currently:
+* collectedHeap.inline.hpp, mmtkHeap.cpp,
+*/
--- src/hotspot/share/gc/mmtk/mmtkHeap.hpp
+++ src/hotspot/share/gc/mmtk/mmtkHeap.hpp
@@ -16,7 +16,7 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contactSUn 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  *
@@ -57,107 +57,106 @@ private:
      
   static HeapWord* allocate_from_tlab(Klass* klass, Thread* thread, size_t size);
  
-  virtual jint initialize();
+  jint initialize();
   
-  virtual HeapWord* mem_allocate(size_t size, bool* gc_overhead_limit_was_exceeded);
+  HeapWord* mem_allocate(size_t size, bool* gc_overhead_limit_was_exceeded);
   
   
   
-  virtual Name kind() const {   
+  Name kind() const {
     return CollectedHeap::MMTkHeap;
   }
-  virtual const char* name() const {
+  const char* name() const {
     return "MMTk";
   }
   
-  virtual size_t capacity() const; 
-  virtual size_t used() const;
+  size_t capacity() const;
+  size_t used() const;
   
-  virtual bool is_maximal_no_gc() const; 
+  bool is_maximal_no_gc() const;
 
-  virtual size_t max_capacity() const; 
-  virtual bool is_in(const void* p) const;
-  virtual bool is_in_reserved(const void* p) const;
+  size_t max_capacity() const;
+  bool is_in(const void* p) const;
+  bool is_in_reserved(const void* p) const;
 
       virtual bool supports_tlab_allocation() const;
 
   // The amount of space available for thread-local allocation buffers.
-  virtual size_t tlab_capacity(Thread *thr) const; 
+  size_t tlab_capacity(Thread *thr) const;
 
   // The amount of used space for thread-local allocation buffers for the given thread.
-  virtual size_t tlab_used(Thread *thr) const; 
+  size_t tlab_used(Thread *thr) const;
   
   
   
-  virtual bool can_elide_tlab_store_barriers() const; 
+  bool can_elide_tlab_store_barriers() const;
 
 
-  virtual bool can_elide_initializing_store_barrier(oop new_obj); 
+  bool can_elide_initializing_store_barrier(oop new_obj);
   
   // mark to be thus strictly sequenced after the stores.
-  virtual bool card_mark_must_follow_store() const; 
+  bool card_mark_must_follow_store() const;
 
-  virtual void collect(GCCause::Cause cause); 
+  void collect(GCCause::Cause cause);
 
   // Perform a full collection
-  virtual void do_full_collection(bool clear_all_soft_refs); 
+  void do_full_collection(bool clear_all_soft_refs);
 
 
   // Return the CollectorPolicy for the heap
-  virtual CollectorPolicy* collector_policy() const ;
+  CollectorPolicy* collector_policy() const ;
 
-  virtual GrowableArray<GCMemoryManager*> memory_managers() ;
-  virtual GrowableArray<MemoryPool*> memory_pools(); 
+  GrowableArray<GCMemoryManager*> memory_managers() ;
+  GrowableArray<MemoryPool*> memory_pools();
 
   // Iterate over all objects, calling "cl.do_object" on each.
-  virtual void object_iterate(ObjectClosure* cl); 
+  void object_iterate(ObjectClosure* cl);
 
   // Similar to object_iterate() except iterates only
   // over live objects.
-  virtual void safe_object_iterate(ObjectClosure* cl) ;
+  void safe_object_iterate(ObjectClosure* cl) ;
 
-  virtual HeapWord* block_start(const void* addr) const ;
+  HeapWord* block_start(const void* addr) const ;
 
-  virtual size_t block_size(const HeapWord* addr) const ;
+  size_t block_size(const HeapWord* addr) const ;
 
-  virtual bool block_is_obj(const HeapWord* addr) const; 
+  bool block_is_obj(const HeapWord* addr) const;
 
-  virtual jlong millis_since_last_gc() ;
+  jlong millis_since_last_gc() ;
 
-
-  virtual void prepare_for_verify() ;
+  void prepare_for_verify() ;
 
 
  private:
 
-  virtual void initialize_serviceability() ;
+  void initialize_serviceability() ;
 
  public:
   
   // Print heap information on the given outputStream.
-  virtual void print_on(outputStream* st) const ;
+  void print_on(outputStream* st) const ;
 
 
   // Print all GC threads (other than the VM thread)
   // used by this heap.
-  virtual void print_gc_threads_on(outputStream* st) const; 
+  void print_gc_threads_on(outputStream* st) const;
 
   // Iterator for all GC threads (other than VM thread)
-  virtual void gc_threads_do(ThreadClosure* tc) const; 
+  void gc_threads_do(ThreadClosure* tc) const;
 
   // Print any relevant tracing info that flags imply.
   // Default implementation does nothing.
-  virtual void print_tracing_info() const ;
+  void print_tracing_info() const ;
 
 
   // An object is scavengable if its location may move during a scavenge.
   // (A scavenge is a GC which is not a full GC.)
-  virtual bool is_scavengable(oop obj);
+  bool is_scavengable(oop obj);
   // Registering and unregistering an nmethod (compiled code) with the heap.
   // Override with specific mechanism for each specialized heap type.
 
   // Heap verification
-  virtual void verify(VerifyOption option);
+  void verify(VerifyOption option);
 
   void post_initialize();
 
